b8bfe18b30a82576828a04f0cce8196b189e7231
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-02-21 08:31:27 -0800

    add kegs value refs on newkegform page

--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
 function NewKegForm(){
+  let _name = null;
+  let _brand = null
+  let _price = null;
+  let _alcoholContent = null;
+  let _availablePints = null;
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    _name.value = '';
+    _brand.value = '';
+    _price.value = '';
+    _alcoholContent.value = '';
+    _availablePints.value = '';
+  }
   return (
     <div className='newkegform-wrapper'>
       <style jsx>{`
@@ -52,37 +66,47 @@ function NewKegForm(){
 
         <h1>Hello <span>Ken's Kegs</span> Employee</h1>
         <h2>Please fill out the following form to add a new Ken Keg</h2>
-        <form>
+        <form onSubmit={handleNewKegFormSubmission}>
           <hr/>
           <input
             class='newkeg-input'
             type='text'
             id='name'
-            placeholder='Keg Name'/>
+            placeholder='Keg Name'
+            ref={(input) => {_name = input;}}
+            />
           <br/>
           <input
             class='newkeg-input'
             type='text'
             id='brand'
-            placeholder='Brand'/>
+            placeholder='Brand'
+            ref={(input) => {_brand = input;}}
+            />
           <br/>
           <input
             class='newkeg-input'
             type='number'
             id='price'
-            placeholder='Price'/>
+            placeholder='Price'
+            ref={(input) => {_price = input;}}
+            />
           <br/>
           <input
             class='newkeg-input'
             type='number'
             id='alcoholContent'
-            placeholder='Alcohol Content'/>
+            placeholder='Alcohol Content'
+            ref={(input) => {_alcoholContent = input;}}
+            />
               <br/>
             <input
               class='newkeg-input'
               type='number'
               id='availablePints'
-              placeholder='Available Pints (Hint: 124)'/>
+              placeholder='Available Pints (Hint: 124)'
+              ref={(input) => {_availablePints = input;}}
+              />
           <br/>
           <button class='newkeg-button'type='submit'>Enter New Keg</button>
         </form>
